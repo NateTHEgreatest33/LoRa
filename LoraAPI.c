fb95adfafb9866546f2bf073aa33cd93a59a7d52
Nate Lenze <nlenze@umich.edu>
2020-09-13 13:43:49 -0500

    Updated all functions and added defines
    to do:
    - verify all magic numbers gone
    - add lora rx function

--- LoraAPI.c
+++ LoraAPI.c
@@ -1,65 +1,11 @@
-/* -----------------------------------------------------------------
-LoRaRead(): Reads from selected register and returns value
-Inputs:
-- (uint8_t) RegisterAddress_8bit: value of register you wish to read from
-Outputs:
-- (uint8_t) Value of register being read from
-Notes:
-- uses PA3 as Chip select manually, PA3 must be configured as GPIO output pin
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/* -----------------------------------------------------------------
- * loRaWrite(): writes to selected register with given data
- * Inputs:
- *   - (uint8_t) RegisterAddress_8bit: value of register you wish to write to
- *   - (uint8_t) data8Bit: data to be writen to register
- * Outputs:
- *   - n/a
- * Notes:
- *   - uses PA3 as Chip select manually, PA3 must be configured as GPIO output pin
- * -------------------------------------------------------------- */
-/* -----------------------------------------------------------------
- * LoraInitTx(): Initilizes LoRa to Tx mode (Tx FIFO ptr, TX highpower mode, HighFreq) 
- * and puts tranciver into sleep mode.
- * Inputs:
- *   - n/a
- * Outputs:
- *   - (bool) successfull or not
- * Notes:
- *   - uses LoRaRead() and LoRaWrite() so their requirements must be met
- * -------------------------------------------------------------- */
-/* -----------------------------------------------------------------
- * LoraSendMessage(): send message via LoRa
- * Inputs:
- *   - (uint8_t) Message[]: to be sent (in byte array)
- *   - (uint8_t) number_of_bytes: number of bytes in Message
- * Outputs:
- *   - (bool) successfull or not (message sent)
- * Notes:
- *   - LoraInitTx OR LoraInitRx should have been ran at some time
- *   - uses LoRaRead() and LoRaWrite() so their requirements must be met
- * Issues:
- *   - Will remain in function untill TxDone interrupt is generated (ie. can get stuck here!!)
- * -------------------------------------------------------------- */
-/* -----------------------------------------------------------------
- * LoraInitRx(): Initilizes LoRa to Rx mode (RX FIFO ptr, TX highpower mode, HighFreq) 
- * and puts tranciver into sleep mode.
- * Inputs:
- *   - n/a
- * Outputs:
- *   - (bool) successfull or not
- * Notes:
- *   - uses LoRaRead() and LoRaWrite() so their requirements must be met
- * -------------------------------------------------------------- */
-
-
 /*********************************************************************
 *
 *   NAME:
 *       loraAPI.c
 *
 *   DESCRIPTION:
-*       API for interfacing Tiva launchpad with LoRa 
-*       requires tiva board support packet files as listed in general
+*       API for interfacing Tiva launchpad with LoRa. 
+*       requires tiva board support package files as listed in general
 *       includes
 *
 *   Copyright 2020 Nate Lenze
@@ -85,26 +31,41 @@ Notes:
 /*--------------------------------------------------------------------
                           LITERAL CONSTANTS
 --------------------------------------------------------------------*/
-#define             CONFIG_REG_ADDR     0x01   /* Config Register Address */
-#define             LORA_FIFO_ADDR      0x00   /* LoRa FIFO Address       */
+/*----------------------------------
+A3 is used as chip select, to change
+what pin is used, modify DATA_PIN
+and DATA_PIN_GROUP defines
+----------------------------------*/
+#define  DATA_PIN            0x08                /* pin 3             */
+#define  DATA_PORT_GROUP     GPIO_PORTA_DATA_R   /* pin register A    */
+
+
+#define  LORA_MAX_POWER_MODE 0xFF                /* max power output  */
+
+#define  LORA_BASE_FIFO_ADD  0x00                /* base fifo address */
+
+#define LORA_SLEEP_MODE      0x80                /* config register 
+                                                    sleep mode        */
+
+#define LORA_STBY_MODE       0x01                /* config register 
+                                                    standby mode      */
+
+
 
 /*--------------------------------------------------------------------
                                 TYPES
 --------------------------------------------------------------------*/
-typedef uint8 lora_modes;       /* Operating Modes                  */
+typedef uint8_t lora_modes;       /* Operating Modes                  */
 enum 
     {
     SLEEP,                      /* sleep mode                       */
     STBY,                       /* standby mode                     */
-    FSTX,                       /* frequency sythesis transmit mode 
-                                   NOT IMPLEMENTED                  */
+    FSTX,                       /* frequency sythesis transmit mode */
     TX,                         /* transmit mode                    */
-    FSRX,                       /* frequency sythesis receive mode
-                                   NOT IMPLEMENTED                  */
+    FSRX,                       /* frequency sythesis receive mode  */
     RXCONTINUOUS,               /* continious receive mode          */
     RXSINGLE,                   /* single receive mode              */
-    CAD                         /* preamble detect mode             
-                                   NOT IMPLEMENTED                  */
+    CAD                         /* preamble detect mode             */
     }; 
 
 typedef uint8_t lora_registers; /* lora registers                   */
@@ -116,8 +77,12 @@ enum
     LORA_FIFO_ADDR_PTR    = 0x0D,  /* pointer to fifo buffer        */
     LORA_TX_FIFO_ADDR     = 0x0E,  /* base addrees for tx fifo      */
     LORA_RX_FIFO_ADDR     = 0x0F,  /* base address for rx fifo      */
-    LOROA_RX_CURR_ADDR    = 0x10   /* current address in buffer for
+    LORA_RX_CURR_ADDR     = 0x10,  /* current address in buffer for
                                       last rx'ed msg                */
+    LORA_FLAGS_MASK       = 0x11,  /* masks for flag register       */
+    LORA_REGISTER_FLAGS   = 0x12,  /* flags register                */
+    LORA_PAYLOAD_SIZE     = 0x22   /* rx payload size register      */
+           
     };
 /*--------------------------------------------------------------------
                            MEMORY CONSTANTS
@@ -183,7 +148,7 @@ while ( number_in_fifo != 0x00 )
 /*----------------------------------------------------------
 Toggle CS low and put request
 ----------------------------------------------------------*/
-GPIO_PORTA_DATA_R &= ~( 0x8 );                      //~(1<<3);//toggle A3 low
+DATA_PORT_GROUP &= ~( DATA_PIN );
 SSIDataPut( SSI0_BASE, register_address );
 
 /*----------------------------------------------------------
@@ -210,7 +175,7 @@ while( SSIBusy( SSI0_BASE ) )
 Pull from fifo and toggle CS
 ----------------------------------------------------------*/
 SSIDataGet( SSI0_BASE, &message_return );
-GPIO_PORTA_DATA_R |= 0x08;                              //toggle A3 high
+DATA_PORT_GROUP |= DATA_PIN;
 
 return message_return;
 
@@ -253,7 +218,7 @@ while ( number_in_fifo != 0x00 )
 /*----------------------------------------------------------
 Toggle CS low and put request
 ----------------------------------------------------------*/
-GPIO_PORTA_DATA_R &= ~(0x8);                                 //~(1<<3);//toggle A3 low
+DATA_PORT_GROUP &= ~(DATA_PIN);
 SSIDataPut(SSI0_BASE, (0x80 | register_address));            //put register value (OR w/ x80 for write)
 
 /*----------------------------------------------------------
@@ -278,7 +243,7 @@ while( SSIBusy( SSI0_BASE ) )
 /*----------------------------------------------------------
 Toggle CS
 ----------------------------------------------------------*/
-GPIO_PORTA_DATA_R |= 0x08;                                  //toggle A3 high
+DATA_PORT_GROUP |= DATA_PIN;
 
 } /* loRa_write_register() */
 
@@ -304,6 +269,7 @@ Local variables
 uint8_t config_register_data; /* configuration data       */
 uint8_t return_data;          /* return data              */
 uint8_t tx_fifo_ptr;          /* tx fifo pointer          */
+uint8_t tx_fifo_ptr_verify;   /* tx fifo pointer verify   */
 uint8_t power_modes;          /* power modes data         */
 
 /*----------------------------------------------------------
@@ -319,18 +285,19 @@ config register bit defintions:
 
 0x80 --> LoRa mode, sleep mode 
 --------------------------------*/
-config_register_data  = 0x80;
+config_register_data  = LORA_SLEEP_MODE;
 tx_fifo_ptr           = 0x00;
+tx_fifo_ptr_verify    = 0x00;
 power_modes           = 0x00;
 
 /*----------------------------------------------------------
 Configure into LoRa sleep mode and verify
 ----------------------------------------------------------*/
-loRaWrite( LORA_REGISTER_OP_MODE, config_register_data );
+loRa_write_register( LORA_REGISTER_OP_MODE, config_register_data );
 
-configRegisterData = loRa_read_register( LORA_REGISTER_OP_MODE ); 
+config_register_data = loRa_read_register( LORA_REGISTER_OP_MODE ); 
 
-if ( configRegisterData != 0x80 )
+if ( config_register_data != LORA_SLEEP_MODE )
     {
     return false;
     }
@@ -338,26 +305,37 @@ if ( configRegisterData != 0x80 )
 /*----------------------------------------------------------
 Configure high power TX mode and verify
 ----------------------------------------------------------*/
-loRaWrite( LORA_REGISTER_POWER, 0xFF );
+loRa_write_register( LORA_REGISTER_POWER, LORA_MAX_POWER_MODE );
 power_modes = loRa_read_register( LORA_REGISTER_POWER );
 
-if( power_modes != 0xFF )
+if( power_modes != LORA_MAX_POWER_MODE )
     {
     return false;
     }
 
 /*----------------------------------------------------------
 Configure TX fifo pointers and verify
-----------------------------------------------------------*/
-loRa_write_register( LORA_FIFO_ADDR_PTR, 0x00);
-tx_fifo_ptr = loRa_read_register( LORA_FIFO_ADDR_PTR );
 
-if( tx_fifo_ptr != 0x80 ){
+Register 0x00 (fifo) can be configured to point to specific
+sections of the fifo buffer. By default tx points to 0x80.
+three registers control this function:
+    - LORA_FIFO_ADDR_PTR: address register 0x00 points to
+    - LORA_TX_FIFO_ADDR: address where TX data starts
+    - LORA_RX_FIFO_ADDR: address where RX data starts
+
+In case LORA_TX_FIFO_ADDR is modified, we always read it
+and set LORA_FIFO_ADDR_PTR accordingly.
+----------------------------------------------------------*/
+tx_fifo_ptr = loRa_read_register ( LORA_TX_FIFO_ADDR );
+loRa_write_register( LORA_FIFO_ADDR_PTR, tx_fifo_ptr );
+
+tx_fifo_ptr_verify = loRa_read_register( LORA_FIFO_ADDR_PTR );
+if( tx_fifo_ptr != tx_fifo_ptr_verify )
+    {
     return false;
-}
+    }
 
 return true;
-
 } /* lora_init_continious_tx() */
 
 /*********************************************************************
@@ -381,6 +359,7 @@ Local variables
 uint8_t config_register_data; /* configuration data       */
 uint8_t return_data;          /* return data              */
 uint8_t rx_fifo_ptr;          /* rx fifo pointer          */
+uint8_t rx_fifo_ptr_verify;   /* rx fifo pointer verify   */
 uint8_t power_modes;          /* power modes data         */
 
 /*----------------------------------------------------------
@@ -396,18 +375,19 @@ config register bit defintions:
 
 0x80 --> LoRa mode, sleep mode 
 --------------------------------*/
-config_register_data  = 0x80;
-tx_fifo_ptr           = 0x00;
+config_register_data  = LORA_SLEEP_MODE;
+rx_fifo_ptr           = 0x00;
+rx_fifo_ptr_verify    = 0x00;
 power_modes           = 0x00;
 
 /*----------------------------------------------------------
 Configure into LoRa sleep mode and verify
 ----------------------------------------------------------*/
-loRaWrite( LORA_REGISTER_OP_MODE, config_register_data );
+loRa_write_register( LORA_REGISTER_OP_MODE, config_register_data );
 
-configRegisterData = loRa_read_register( LORA_REGISTER_OP_MODE ); 
+config_register_data = loRa_read_register( LORA_REGISTER_OP_MODE ); 
 
-if ( configRegisterData != 0x80 )
+if ( config_register_data != LORA_SLEEP_MODE )
     {
     return false;
     }
@@ -415,87 +395,39 @@ if ( configRegisterData != 0x80 )
 /*----------------------------------------------------------
 Configure high power TX mode and verify
 ----------------------------------------------------------*/
-loRaWrite( LORA_REGISTER_POWER, 0xFF );
+loRa_write_register( LORA_REGISTER_POWER, LORA_MAX_POWER_MODE );
 power_modes = loRa_read_register( LORA_REGISTER_POWER );
 
-if( power_modes != 0xFF )
+if( power_modes != LORA_MAX_POWER_MODE )
     {
     return false;
     }
 
 /*----------------------------------------------------------
 Configure RX fifo pointers and verify
-----------------------------------------------------------*/
-loRa_write_register( LORA_FIFO_ADDR_PTR, 0x00);
-tx_fifo_ptr = loRa_read_register( LORA_FIFO_ADDR_PTR );
 
-if( tx_fifo_ptr != 0x80 ){
+Register 0x00 (fifo) can be configured to point to specific
+sections of the fifo buffer. By default rx points to 0x00.
+three registers control this function:
+    - LORA_FIFO_ADDR_PTR: address register 0x00 points to
+    - LORA_TX_FIFO_ADDR: address where TX data starts
+    - LORA_RX_FIFO_ADDR: address where RX data starts
+
+In case LORA_RX_FIFO_ADDR is modified, we always read it
+and set LORA_FIFO_ADDR_PTR accordingly.
+----------------------------------------------------------*/
+rx_fifo_ptr = loRa_read_register ( LORA_RX_FIFO_ADDR );
+loRa_write_register( LORA_FIFO_ADDR_PTR, rx_fifo_ptr );
+
+rx_fifo_ptr_verify = loRa_read_register( LORA_FIFO_ADDR_PTR );
+if( rx_fifo_ptr != rx_fifo_ptr_verify )
+    {
     return false;
-}
+    }
 
 return true;
 
 } /* lora_init_rx() */
-
-// /* -----------------------------------------------------------------
-//  * LoRaMode(): sets configuration register to specificed mode
-//  * Inputs:
-//  *   - (uint8_t) mode: what mode to set, (See enum 'modes' at top of file)
-//  * Outputs:
-//  *   - (bool) successfull or not
-//  * Notes:
-//  *   - uses LoRaRead() and LoRaWrite() so their requirements must be met
-//  * -------------------------------------------------------------- */
-// bool LoRaMode(uint8_t mode){
-// 	uint8_t configReg = 0x00;
-// 	if(mode == SLEEP){
-// 		configReg = loRaRead(0x01);
-// 		configReg &= ~(0x7);
-// 		loRaWrite(0x01, configReg);
-// 		if(loRaRead(0x01) != configReg){
-// 			return false;
-// 		}
-// 		return true;
-// 	}else if(mode == STBY){
-// 		configReg = loRaRead(0x01);
-// 		configReg &= ~(0x7);
-// 		configReg |= 0x01;
-// 		loRaWrite(0x01, configReg);
-// 		if(loRaRead(0x01) != configReg){
-// 			return false;
-// 		}
-// 		return true;
-// 	}else if(mode == TX){
-// 		configReg = loRaRead(0x01);
-// 		configReg &= ~(0x7);
-// 		configReg |= 0x03;
-// 		loRaWrite(0x01, configReg);
-// 		if(loRaRead(0x01) != configReg){
-// 			return false;
-// 		}
-// 		return true;
-// 	}else if(mode == RXCONTINUOUS){
-// 		configReg = loRaRead(0x01);
-// 		configReg &= ~(0x7);
-// 		configReg |= 0x05;
-// 		loRaWrite(0x01, configReg);
-// 		if(loRaRead(0x01) != configReg){
-// 			return false;
-// 		}
-// 		return true;
-// 	}else if(mode == RXSINGLE){
-// 		configReg = loRaRead(0x01);
-// 		configReg &= ~(0x7);
-// 		configReg |= 0x06;
-// 		loRaWrite(0x01, configReg);
-// 		if(loRaRead(0x01) != configReg){
-// 			return false;
-// 		}
-// 		return true;
-// 	}else{
-// 		return false;
-// 	}
-// }
 
 /*********************************************************************
 *
@@ -506,53 +438,95 @@ return true;
 *       send message
 *
 *********************************************************************/
-bool lora_send_message(uint8_t Message[], uint8_t number_of_bytes){
-	//local variables
-	uint8_t configRegisterData = 0x00;
-	uint8_t FifoPtrAddr = 0x00;
-	int i;
-	
-	//Put Tranciver in stby mode to fill fifo
-	configRegisterData = loRaRead(CONFIG_REG_ADDR);
-	configRegisterData |= 0x1;
-	loRaWrite(CONFIG_REG_ADDR, configRegisterData);
-	
-	//reset Tx FifoPtr
-	FifoPtrAddr = loRaRead(0x0E); //(TX fifo Base)
-	loRaWrite(0x0D,FifoPtrAddr);//point 0x00 FIFO to TX location
-	if(loRaRead(0x0D) != FifoPtrAddr){
-		return false;
-	}
-	
-	//Fill fifo
-	for(i = 0; i < number_of_bytes; i++){
-		loRaWrite(0x00, Message[i]);
-	}
-	
-	//set payload length to numBytes
-	loRaWrite(0x22,number_of_bytes);
-	if(loRaRead(0x22) != number_of_bytes){
-		return false;
-	}
-	
-	//Set to Tx Mode
-	configRegisterData = 0x83; //set last two bits high (Tx mode)
-	loRaWrite(CONFIG_REG_ADDR,configRegisterData);
-	if(loRaRead(CONFIG_REG_ADDR) != 0x83){
-		return false;
-	}
-	
-	//wait until TxDone sent
-	while((loRaRead(0x12) & 0x08) != 0x08)
-		{
-	  }
-	
-	//Clear flags
-	loRaWrite(0x12, 0x08); //clear IRQ
-	if(loRaRead(0x12)!= 0x00){
-		return false;
-	}
+bool lora_send_message
+    (
+    uint8_t Message[],                    /* array of bytes to send */
+    uint8_t number_of_bytes               /* size of array          */
+    )
+{
+/*----------------------------------------------------------
+Local variables
+----------------------------------------------------------*/
+uint8_t config_register_data;   /* config register data   */
+uint8_t fifo_ptr_address;       /* fifo pointer address   */
+int i;                          /* interator              */
 
-	return true;
-}
+/*----------------------------------------------------------
+Initilize local variables
+----------------------------------------------------------*/
+config_register_data  = 0x00;
+fifo_ptr_address      = 0x00;
+i                     = 0x00;
+	
+/*----------------------------------------------------------
+Put into standby mode to fill fifo
+----------------------------------------------------------*/
+config_register_data = loRa_read_register( LORA_REGISTER_OP_MODE );
+config_register_data |= LORA_STBY_MODE;
+loRa_write_register( LORA_REGISTER_OP_MODE, config_register_data);
 
+/*----------------------------------------------------------
+Reset TX fifo 
+----------------------------------------------------------*/
+fifo_ptr_address = loRa_read_register( LORA_TX_FIFO_ADDR );
+loRa_write_register( LORA_FIFO_ADDR_PTR, fifo_ptr_address );
+
+if( loRa_read_register( LORA_FIFO_ADDR_PTR ) != fifo_ptr_address )
+    {
+    return false;
+    }
+
+/*----------------------------------------------------------
+Fill in fifo
+----------------------------------------------------------*/
+for( i = 0; i < number_of_bytes; i++ )
+    {
+    loRa_write_register( LORA_REGISTER_FIFO, Message[i] );
+    }
+
+/*----------------------------------------------------------
+Set payload length to numBytes and verify
+----------------------------------------------------------*/
+loRa_write_register( LORA_PAYLOAD_SIZE, number_of_bytes );
+if( loRa_read_register( LORA_PAYLOAD_SIZE ) != number_of_bytes )
+    {
+    return false;
+    }
+
+/*----------------------------------------------------------
+Set into TX mode
+----------------------------------------------------------*/
+config_register_data = 0x83;
+loRa_write_register( LORA_REGISTER_OP_MODE, config_register_data );
+if( loRa_read_register( LORA_REGISTER_OP_MODE ) != 0x83 )
+    {
+    return false;
+    }
+
+/*----------------------------------------------------------
+Wait for TX to complete
+----------------------------------------------------------*/
+while( ( loRa_read_register( LORA_REGISTER_FLAGS ) & 0x08 ) != 0x08)
+    {
+    }
+/*----------------------------------------------------------
+Clear IRQ flags
+----------------------------------------------------------*/
+loRa_write_register( LORA_REGISTER_FLAGS, 0x08 );
+if( loRa_read_register( LORA_REGISTER_FLAGS ) != 0x00 )
+    {
+    return false;
+    }
+
+return true;
+} /* lora_send_message() */
+
+/*********************************************************************
+*
+*   PROCEDURE NAME:
+*       lora_rx_message
+*
+*   DESCRIPTION:
+*       recive message
+*
+*********************************************************************/
